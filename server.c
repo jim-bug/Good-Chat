8461591a32fef45defa529f1bb216d67e3ad20ff
jim_bug <ignazioandsperandeo@gmail.com>
2024-04-30 23:46:35 +0200

    Aggiunta selezione della chat, previste due opzioni server e client, se si sceglie client si devono passare ip del server e numero di porta

--- server.c
+++ server.c
@@ -4,63 +4,57 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "chat.h"
 
-typedef struct MSG {
-    int value;
-    char tipo[150];
-} MSGI;
 
 int main(int argc, char* argv[]) {
-    int server_sock;
-    struct sockaddr_in server, client;
-    int length = sizeof(client);
-    int complete_socket;
-    MSGI msg;
+    if(strcmp(argv[1], "-s") == 0){
+	    int server_sock;
+	    struct sockaddr_in server, client;
+	    int length = sizeof(client);
+	    int complete_socket;
+	    server_sock = socket(AF_INET, SOCK_STREAM, 0);              //  l'OS associa al socket un file descriptor con le seguenti caratteristiche.
+	    if (server_sock < 0) {
+        		perror("Creazione della socket -> Errore");
+        		exit(-1);
+	    }
 
-    server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock < 0) {
-        perror("Creazione della socket -> Errore");
-        exit(-1);
+	    // Popolazione delle informazioni del mezzo socket dedicato al server, quindi ipv4 + porta e famiglia di indirizzi Ipv4.
+	    server.sin_family = AF_INET;
+	    server.sin_addr.s_addr = INADDR_ANY;
+	    server.sin_port = htons(SERVER_PORT);
+
+	    // associo al file descriptor la struct con tutti gli argomenti in erenti al mezzo socket del server
+	    if (bind(server_sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+		        perror("Binding server -> Errore");
+	        	exit(-1);
+    	    }
+
+	    if (getsockname(server_sock, (struct sockaddr *)&server, &length) < 0) {
+		        perror("Errore con getsockname()");
+		        exit(-1);
+	    }
+
+	    // metto in ascolto con una coda massima di 1, il mezzo socket dedicato al server.
+	    if (listen(server_sock, 1) < 0) {
+        		perror("Listen -> Errore");
+		        exit(-1);
+	    }
+
+	    complete_socket = accept(server_sock, (struct sockaddr *)&client, (socklen_t *)&length);
+	    if (complete_socket < 0) {
+		        perror("Errore accept");
+	                exit(-1);
+            }
+
     }
-
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(SERVER_PORT);
-
-    if (bind(server_sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        perror("Binding server -> Errore");
-        exit(-1);
+    else if(strcmp(argv[1], "-c") == 0 && argc == 4){	// specifico uguale a 4 perchè ho bisogno dell'ip e del numero di porta del server.
+	printf("Ancora da implementare!");
     }
-
-    if (getsockname(server_sock, (struct sockaddr *)&server, &length) < 0) {
-        perror("Errore con getsockname()");
-        exit(-1);
-    }
-
-    if (listen(server_sock, 1) < 0) {
-        perror("Listen -> Errore");
-        exit(-1);
-    }
-    char message[150];
-//    while (1) {
-        complete_socket = accept(server_sock, (struct sockaddr *)&client, (socklen_t *)&length);
-        if (complete_socket < 0) {
-            perror("Errore accept");
-            exit(-1);
-        }
-while(1){
-        // Gestione della connessione
-        ssize_t bytes_read = read(complete_socket, message, 150);
-        if (bytes_read <= 0) {
-            printf("Connessione chiusa dal client %ld\n", bytes_read);
-            close(complete_socket);
-	    break;
-        }
-
-        printf("Client: %s\n", message);
-
-//        close(complete_socket); // Chiudi il socket quando hai finito di gestire la connessione
+    else{
+	printf("Operazione non valida o non specificata!");
+	exit(-1);
     }
 
     return 0;
