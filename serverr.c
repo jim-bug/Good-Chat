735e943f61ca8cd45932e0fe9c5e00161032fdd9
jim_bug <ignazioandsperandeo@gmail.com>
2024-05-03 00:49:17 +0200

    Aggiunta due finestre con ncurses, una per l'inserimento del messaggio da inviare al server ed una per visualizzare ciò che il server invia. Reso generale la scelta dell'ip a cui si deve connettere il client.

--- serverr.c
+++ serverr.c
@@ -7,34 +7,37 @@
 #include <pthread.h>
 #include <netdb.h>
 #include <ncurses.h>
+#include <arpa/inet.h>
 
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // Inizializzazione del mutex
-WINDOW *win;
+// pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // Inizializzazione del mutex
+WINDOW* input_window;
+WINDOW* output_window;
 
-int y_prompt = 2;
-int x_prompt = 70;
-int i = -1;
+void create_window(WINDOW** new_win, int width, int height, int x, int y){
+	*new_win = newwin(height, width, y, x);
+        refresh();
+        box(*new_win, '|', '|');
+        wrefresh(*new_win);
+}
 
 void* get_message_from_host(void* arg) {
     int sockfd = *((int*)arg);
-    char buf[150]; // Buffer per i dati
+    int row = 1;
+    char buf[300]; // Buffer per i dati
     while (1) {
         ssize_t bytes_read = read(sockfd, buf, sizeof(buf));
         if (bytes_read < 0) {
             perror("Errore nella lettura dal sockettttttt");
             break;
         } else if (bytes_read == 0) {
-//            close(sockfd);
             break;
         }
         buf[bytes_read] = '\0';
-       
-        pthread_mutex_lock(&mutex);
-         i++;
-        pthread_mutex_unlock(&mutex);
-        mvprintw(i, 1, "Client: %s", buf);
-       refresh();
+//        snprintf(buf, 300, "Client: %s", buf);
+        mvwprintw(output_window, row, 1, "Client> %s", buf);
+        wrefresh(output_window);
+        row ++;
     }
 
     return NULL;
@@ -42,21 +45,21 @@ void* get_message_from_host(void* arg) {
 
 void* send_message_to_host(void* arg) {
     int sockfd = *((int*)arg);
+    int row = 1;
     char buf[150];
 
     while (1) {
+        mvwprintw(input_window, row, 1, "Me>");
+        mvwgetstr(input_window, row, 4, buf);
         buf[strcspn(buf, "\n")] = '\0';
+        wrefresh(input_window);
         ssize_t bytes_written = write(sockfd, buf, strlen(buf));
         if (bytes_written < 0) {
             perror("Errore nella scrittura sul socket 2");
             break;
         }
-        pthread_mutex_lock(&mutex);
-	i++;
-        pthread_mutex_unlock(&mutex);
-        mvprintw(i, 1, "Inserisci il messaggio da inviare al client: ");
-        mvscanw(i, 70, "%s", buf);
-        refresh();
+
+        row ++;
     }
 
     return NULL;
@@ -64,12 +67,14 @@ void* send_message_to_host(void* arg) {
 
 int main(int argc, char* argv[]) {
     initscr();
-    win = newwin(24, 80, 0, 0);
-
+    int y, x;
+    getmaxyx(stdscr, y, x);
+    create_window(&input_window, x/2, y, 0, 0);
+    create_window(&output_window, x/2, y, x/2, 0);
     pthread_t receive_thread;
     pthread_t write_thread;
 
-    if (argc != 2 || (strcmp(argv[1], "-s") != 0 && strcmp(argv[1], "-c") != 0)) {
+    if ((strcmp(argv[1], "-s") != 0 && strcmp(argv[1], "-c"))) {
         fprintf(stderr, "Utilizzo: %s -s|-c\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -83,6 +88,7 @@ int main(int argc, char* argv[]) {
         server_sock = socket(AF_INET, SOCK_STREAM, 0);
         if (server_sock < 0) {
             perror("Errore nella creazione del socket del server");
+            endwin();
             exit(EXIT_FAILURE);
         }
 
@@ -95,12 +101,14 @@ int main(int argc, char* argv[]) {
         // Binding del socket del server all'indirizzo locale
         if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
             perror("Errore nel binding del socket del server");
+            endwin();
             exit(EXIT_FAILURE);
         }
 
         // Mette il socket del server in ascolto di connessioni
         if (listen(server_sock, 1) < 0) {
             perror("Errore nella messa in ascolto del socket del server");
+            endwin();
             exit(EXIT_FAILURE);
         }
 
@@ -108,30 +116,17 @@ int main(int argc, char* argv[]) {
         client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_len);
         if (client_sock < 0) {
             perror("Errore nell'accettazione della connessione");
+            endwin();
             exit(EXIT_FAILURE);
         }
+        
 
         // Creazione dei thread per la ricezione e l'invio dei messaggi
-        if (pthread_create(&receive_thread, NULL, get_message_from_host, &client_sock) != 0) {
-            perror("Errore durante la creazione del thread di ricezione");
-            exit(EXIT_FAILURE);
-        }
-
-        if (pthread_create(&write_thread, NULL, send_message_to_host, &client_sock) != 0) {
-            perror("Errore durante la creazione del thread di invio");
-            exit(EXIT_FAILURE);
-        }
-
-        // Attesa del completamento dei thread
-        if (pthread_join(receive_thread, NULL) != 0) {
-            perror("Errore durante l'attesa del thread di ricezione");
-            exit(EXIT_FAILURE);
-        }
-
-        if (pthread_join(write_thread, NULL) != 0) {
-            perror("Errore durante l'attesa del thread di invio");
-            exit(EXIT_FAILURE);
-        }
+        pthread_create(&receive_thread, NULL, get_message_from_host, &client_sock);
+        pthread_create(&write_thread, NULL, send_message_to_host, &client_sock);
+        pthread_join(receive_thread, NULL);
+        pthread_join(write_thread, NULL);
+        wrefresh(input_window);
     } else if (strcmp(argv[1], "-c") == 0) {
         int client_sock;
         struct sockaddr_in server_addr;
@@ -141,50 +136,38 @@ int main(int argc, char* argv[]) {
         client_sock = socket(AF_INET, SOCK_STREAM, 0);
         if (client_sock < 0) {
             perror("Errore nella creazione del socket del client");
+            endwin();
             exit(EXIT_FAILURE);
         }
 
         // Risoluzione dell'indirizzo IP del server
-        hp = gethostbyname("localhost");
-        if (hp == NULL) {
-            fprintf(stderr, "Errore nella risoluzione dell'host\n");
-            exit(EXIT_FAILURE);
-        }
+        
 
         // Inizializzazione dell'indirizzo del server
         memset(&server_addr, 0, sizeof(server_addr));
         server_addr.sin_family = AF_INET;
-        memcpy(&server_addr.sin_addr, hp->h_addr, hp->h_length);
+        // memcpy(&server_addr.sin_addr, hp->h_addr, hp->h_length);
         server_addr.sin_port = htons(8085);
+        inet_pton(AF_INET, argv[2], &server_addr.sin_addr);
 
         // Connessione al server
         if (connect(client_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
             perror("Errore durante la connessione al server");
+            endwin();
             exit(EXIT_FAILURE);
         }
 
         // Creazione dei thread per la ricezione e l'invio dei messaggi
-        if (pthread_create(&receive_thread, NULL, get_message_from_host, &client_sock) != 0) {
-            perror("Errore durante la creazione del thread di ricezione");
-            exit(EXIT_FAILURE);
-        }
+        pthread_create(&receive_thread, NULL, get_message_from_host, &client_sock);
+        pthread_create(&write_thread, NULL, send_message_to_host, &client_sock);
+        pthread_join(receive_thread, NULL);
+        pthread_join(write_thread, NULL);
 
-        if (pthread_create(&write_thread, NULL, send_message_to_host, &client_sock) != 0) {
-            perror("Errore durante la creazione del thread di invio");
-            exit(EXIT_FAILURE);
-        }
-
-        // Attesa del completamento dei thread
-        if (pthread_join(receive_thread, NULL) != 0) {
-            perror("Errore durante l'attesa del thread di ricezione");
-            exit(EXIT_FAILURE);
-        }
-
-        if (pthread_join(write_thread, NULL) != 0) {
-            perror("Errore durante l'attesa del thread di invio");
-            exit(EXIT_FAILURE);
-        }
     }
+    wrefresh(input_window);
+    wrefresh(output_window);
+    delwin(input_window);
+    delwin(output_window);
     endwin();
     return 0;
 }
